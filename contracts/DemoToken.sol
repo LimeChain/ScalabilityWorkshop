8da99ca84fc5a8664d5f57555c3114728b0c0aa2
George Spasov <spasov05203@gmail.com>
2019-08-09 16:02:14 +0300

    Unit testing all scenarios

--- contracts/DemoToken.sol
+++ contracts/DemoToken.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.0;
 
+import "@openzeppelin/contracts/cryptography/ECDSA.sol";
+
 contract DemoToken {
 
   struct Item {
@@ -10,6 +12,7 @@ contract DemoToken {
   Item[] public items;
 
   mapping(uint256 => address) public ownerOf;
+  mapping(uint256 => uint256) public nonceOf;
 
   constructor(bytes32 name, bytes memory image) public {
     Item memory i = Item({
@@ -20,6 +23,22 @@ contract DemoToken {
     items.push(i);
 
     ownerOf[0] = msg.sender;
+  }
+
+  function safeTransfer(uint256 id, address receiver, uint256 price, bytes memory signature) public payable {
+    require(id < items.length, "Invalid item");
+    require(msg.value == price, "Not enough price sent");
+
+    uint256 nonce = nonceOf[id];
+    bytes32 sendingHash = keccak256(abi.encodePacked(id, receiver, price, nonce));
+    address recoveredAddress = ECDSA.recover(ECDSA.toEthSignedMessageHash(sendingHash), signature);
+
+    require(recoveredAddress == ownerOf[id], "Invalid message or message signature");
+
+    ownerOf[id] = receiver;
+    address payable wallet = address(uint160(address(recoveredAddress)));
+    wallet.transfer(price);
+    nonceOf[id]++;
   }
   
 }
--- test/demoTokenTest.js
+++ test/demoTokenTest.js
@@ -1,13 +1,25 @@
 const etherlime = require('etherlime-lib');
+const ethers = require('ethers');
 const DemoToken = require('../build/DemoToken.json');
 
 
 describe('Example', () => {
     let aliceAccount = accounts[3];
+    let bobsAccount = accounts[4];
+    let carolAccount = accounts[5];
     let deployer;
     let demoTokenInstance;
     const name = ethers.utils.formatBytes32String("Placeholder");
     const image = ethers.utils.hexlify(ethers.utils.toUtf8Bytes("Qmcu7QgaAt28skUNFje1Nb6CyvWt888meddyBAipWKpBDT"));
+    const ETH = ethers.utils.parseEther("1");
+
+    const signMessage = (wallet, id, receiver, price, nonce) => {
+
+        const hashMsg = ethers.utils.solidityKeccak256(['uint256', 'address', 'uint256', 'uint256'], [id, receiver, price, nonce]);
+        var hashData = ethers.utils.arrayify(hashMsg);
+        const signature = wallet.signMessage(hashData);
+        return signature;
+    }
 
     beforeEach(async () => {
         deployer = new etherlime.EtherlimeGanacheDeployer(aliceAccount.secretKey);
@@ -21,6 +33,95 @@ describe('Example', () => {
 
         const owner = await demoTokenInstance.ownerOf(0);
         assert.strictEqual(owner, aliceAccount.signer.address, "Owner was not set to alice");
+    });
+
+
+    it('should be able to transfer with signed message', async () => {
+        const id = 0;
+        const nonce = await demoTokenInstance.nonceOf(id);
+        const signature = signMessage(aliceAccount.signer, id, bobsAccount.signer.address, ETH, nonce);
+        await demoTokenInstance.safeTransfer(id, bobsAccount.signer.address, ETH, signature, { value: ETH });
+
+        const owner = await demoTokenInstance.ownerOf(id);
+        assert.strictEqual(owner, bobsAccount.signer.address, "Owner was not set to bob");
+    });
+
+    it('should be able to transfer with signed message once again', async () => {
+        const id = 0;
+        let nonce = await demoTokenInstance.nonceOf(id);
+        let signature = signMessage(aliceAccount.signer, id, bobsAccount.signer.address, ETH, nonce);
+        await demoTokenInstance.safeTransfer(id, bobsAccount.signer.address, ETH, signature, { value: ETH });
+
+        nonce = await demoTokenInstance.nonceOf(id);
+        signature = signMessage(bobsAccount.signer, id, carolAccount.signer.address, ETH, nonce);
+        await demoTokenInstance.safeTransfer(id, carolAccount.signer.address, ETH, signature, { value: ETH });
+
+        const owner = await demoTokenInstance.ownerOf(id);
+        assert.strictEqual(owner, carolAccount.signer.address, "Owner was not set to carol");
+
+    });
+
+    it('should not be able to transfer without correct signed message', async () => {
+        const id = 0;
+        const nonce = await demoTokenInstance.nonceOf(id);
+        const signature = signMessage(carolAccount.signer, id, bobsAccount.signer.address, ETH, nonce);
+        await assert.revert(demoTokenInstance.safeTransfer(id, bobsAccount.signer.address, ETH, signature, { value: ETH }));
+
+        const owner = await demoTokenInstance.ownerOf(id);
+        assert.strictEqual(owner, aliceAccount.signer.address, "Owner was not still alice");
+    });
+
+    it('should not be able to transfer without correct nonce', async () => {
+        const id = 0;
+        const signature = signMessage(aliceAccount.signer, id, bobsAccount.signer.address, ETH, 14);
+        await assert.revert(demoTokenInstance.safeTransfer(id, bobsAccount.signer.address, ETH, signature, { value: ETH }));
+
+        const owner = await demoTokenInstance.ownerOf(id);
+        assert.strictEqual(owner, aliceAccount.signer.address, "Owner was not still alice");
+    });
+
+    it('should not be able to transfer without correct receiver', async () => {
+        const id = 0;
+        const nonce = await demoTokenInstance.nonceOf(id);
+        const signature = signMessage(aliceAccount.signer, id, carolAccount.signer.address, ETH, nonce);
+        await assert.revert(demoTokenInstance.safeTransfer(id, bobsAccount.signer.address, ETH, signature, { value: ETH }));
+
+        const owner = await demoTokenInstance.ownerOf(id);
+        assert.strictEqual(owner, aliceAccount.signer.address, "Owner was not still alice");
+    });
+
+    it('should not be able to transfer without correct price', async () => {
+        const id = 0;
+        const nonce = await demoTokenInstance.nonceOf(id);
+        const signature = signMessage(aliceAccount.signer, id, bobsAccount.signer.address, ETH, nonce);
+        await assert.revert(demoTokenInstance.safeTransfer(id, bobsAccount.signer.address, '100', signature, { value: ETH }));
+
+        const owner = await demoTokenInstance.ownerOf(id);
+        assert.strictEqual(owner, aliceAccount.signer.address, "Owner was not still alice");
+    });
+
+    it('should not be able to transfer without sending price', async () => {
+        const id = 0;
+        const nonce = await demoTokenInstance.nonceOf(id);
+        const signature = signMessage(aliceAccount.signer, id, bobsAccount.signer.address, ETH, nonce);
+        await assert.revert(demoTokenInstance.safeTransfer(id, bobsAccount.signer.address, ETH, signature));
+
+        const owner = await demoTokenInstance.ownerOf(id);
+        assert.strictEqual(owner, aliceAccount.signer.address, "Owner was not still alice");
+    });
+
+    it('should not be able to transfer from the previous owner', async () => {
+        const id = 0;
+        let nonce = await demoTokenInstance.nonceOf(id);
+        let signature = signMessage(aliceAccount.signer, id, bobsAccount.signer.address, ETH, nonce);
+        await demoTokenInstance.safeTransfer(id, bobsAccount.signer.address, ETH, signature, { value: ETH });
+
+        nonce = await demoTokenInstance.nonceOf(id);
+        signature = signMessage(aliceAccount.signer, id, carolAccount.signer.address, ETH, nonce);
+        await assert.revert(demoTokenInstance.safeTransfer(id, carolAccount.signer.address, ETH, signature, { value: ETH }));
+
+        const owner = await demoTokenInstance.ownerOf(id);
+        assert.strictEqual(owner, bobsAccount.signer.address, "Owner was not set to bob");
     });
 
 
